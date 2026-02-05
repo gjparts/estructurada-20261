e5c5369761beb43c5dfe81ee5b5065ffc36c81d1
GJParts <39961515+gjparts@users.noreply.github.com>
2026-02-05 10:53:03 -0600

    commit 0502202627209

---
+++ 04_Captura2/04_Captura2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04_Captura2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.Write("a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("b: ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("c: ");
+            c = double.Parse(Console.ReadLine());
+
+            //hay muchas formas de resolverlo
+            //forma 1:
+            Console.WriteLine($"resultado es: {(a+b)/(a-c)}");
+            //forma 2:
+            double numerador = a+b, denominador = a-c;
+            Console.WriteLine($"resultado es: {numerador/denominador}");
+            //forma 3:
+            double res = (a + b) / (a - c);
+            Console.WriteLine($"resultado es: {res}");
+            //forma 4:
+            res = numerador / denominador;
+            Console.WriteLine($"resultado es: {res}");
+        }
+    }
+}
---
+++ 05_Captura3/05_Captura3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _05_Captura3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d;
+            Console.Write("a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("b: ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("c: ");
+            c = double.Parse(Console.ReadLine());
+            Console.Write("d: ");
+            d = double.Parse(Console.ReadLine());
+
+            //forma 1: todo de un solo
+            Console.WriteLine($"resultado: {Math.Sqrt((a+b)/Math.Pow(c,3))*d/8.0}");
+
+            //forma 2: separar el problema en partes
+            //Ustedes deciden como van a seccionar las partes del problema
+            double raiz, numerador, denomidador, res;
+            numerador = a + b;
+            denomidador = Math.Pow(c, 3);
+            raiz = Math.Sqrt(numerador/denomidador);
+            res = raiz * d / 8.0;
+            Console.WriteLine($"resultado: {res}");
+        }
+    }
+}
