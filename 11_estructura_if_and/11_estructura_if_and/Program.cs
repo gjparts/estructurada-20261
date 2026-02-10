f919f85988d487acc526559e58ac35e4c5f73ac3
GJParts <39961515+gjparts@users.noreply.github.com>
2026-02-10 11:22:06 -0600

    commit 100220261839

---
+++ 11_estructura_if_and/11_estructura_if_and/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _11_estructura_if_and
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Operador logico AND
+             Hace obligatorio el cumplimiento de una o varias condiciones
+            en C# y C++ se le representa con doble ampersand &&
+            ejemplo:
+            Solicite al usuario que digite los enteros para a, b, c.
+            Si a es mayor a 7 Y b es menor a 12 Y c es igual a 11 entonces
+            muestre el mensaje: COMBINACION CORRECTA, de lo contrario
+            mostrara el mensaje: ACCESO NO VALIDO
+            -> Resuelvalo utilizando operadores logicos.*/
+            try
+            {
+                int a, b, c;
+                Console.Write("a: ");
+                a = int.Parse(Console.ReadLine());
+                Console.Write("b: ");
+                b = int.Parse(Console.ReadLine());
+                Console.Write("c: ");
+                c = int.Parse(Console.ReadLine());
+                if( a > 7 && b < 12 && c == 11 )
+                    Console.WriteLine("COMBINACION CORRECTA");
+                else
+                    Console.WriteLine("ACCESO NO VALIDO");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Solo se permite numeros enteros.");
+            }
+        }
+    }
+}
