b0f1a9a3768954015a4f35d02623dc44ef492c5e
GJParts <39961515+gjparts@users.noreply.github.com>
2026-02-16 10:59:51 -0600

    commit 1602202619188

---
+++ 13_estructura_if_or/13_estructura_if_or/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _13_estructura_if_or
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Operador Logico OR
+             Devuelve true se se cumple al menos una de las expresiones
+            de comparacion cuando hay dos o mas de estas expresiones.
+            En C# y C++ se usan dos barras || para referirse a OR
+            El caracter barra se puede obtener con la combinacion Alt+124 en Windows
+            
+            ejemplo:
+            Haga un programa que pregunte el mes y el año como numeros enteros.
+            el programa va a imprimir la cantidad de dias que tiene el mes/año
+            digitados.
+            Valide que el mes sea un numero entre 1 y 12 de lo contrario imprimir
+            que el mes no es valido.*/
+            try
+            {
+                int m, a;
+                Console.Write("Digite el mes: ");
+                m = int.Parse(Console.ReadLine());
+                Console.Write("Digite el año: ");
+                a = int.Parse(Console.ReadLine());
+
+                if (m >= 1 && m <= 12)
+                {
+                    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+                        Console.WriteLine("31 dias");
+
+                    if (m == 4 || m == 6 || m == 9 || m == 11)
+                        Console.WriteLine("30 dias");
+
+                    if (m == 2)
+                    {
+                        if (a % 4 == 0)
+                            Console.WriteLine("29 dias");
+                        else
+                            Console.WriteLine("28 dias");
+                    }
+                }
+                else
+                    Console.WriteLine("Mes no es valido");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Debe digitar numeros");
+            }
+        }
+    }
+}
---
+++ 14_estructura_if_or_and/14_estructura_if_or_and/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _14_estructura_if_or_and
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*En un negocio se va a dar un descuento del 10% para lo vendido a personas
+            con edades entre 18 y 24 ó con edades entre 40 y 60.
+            Capture el monto de la venta y la edad, luego imprima el
+            monto del descuento y el total a pagar.*/
+            try
+            {
+                float venta, descuento = 0;
+                int edad;
+                Console.Write("Digite la edad del cliente: ");
+                edad = int.Parse(Console.ReadLine());
+                Console.Write("Digite el monto de la venta: ");
+                venta = float.Parse(Console.ReadLine());
+
+                if (edad >= 18 && edad <= 24 || edad >= 40 && edad <= 60)
+                    descuento = venta * 0.10f;
+
+                Console.WriteLine($"El descuento es: {descuento}");
+                Console.WriteLine($"El total a pagar es: {venta-descuento}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Solo se acepta numeros.");
+            }
+        }
+    }
+}
