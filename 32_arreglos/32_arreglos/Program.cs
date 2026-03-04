91bf0a3d4336e6a09de98582eb458d1cf99c6be8
gjparts <gjparts@gmail.com>
2026-03-04 10:54:53 -0600

    commit 0403202610561

---
+++ 32_arreglos/32_arreglos/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _32_arreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //consideraciones en C#
+            //Conversion explicita: consiste en decirle al compilador
+            //que el siguiente valor lo considere como determinado tipo.
+            //no funciona en la mayoria de los casos; pero con tipos
+            //primitivos es muy efectivo.
+            //ejemplos:
+            double a = 5.6;
+            int b = (int)a;
+            Console.WriteLine($"valor de a: {a}");
+            Console.WriteLine($"valor de b: {b}");
+
+            double x = 2.5354345354345354222222222223;
+            float y = (float)x;
+
+            //siempre recuerde que toda conversion explicita
+            //representa perdida de precision.
+
+            //consideraciones respecto al foreach:
+            double[] arreglo = { 2.3, 4.456, 777, -3.2, 78, 123.432 };
+            //foreach no permite modificar el valor del elemento de un arreglo
+            
+            //ejemplo: para el arreglo anterior reemplace cada
+            //uno de sus valores multiplicandolo por 10
+            
+            //primer intento: usando foreach (no se puede modificar item)
+            /*foreach(double item in arreglo)
+            {
+                item = item * 10;
+            }*/
+
+            //segundo intento: usando for tradicional
+            for( int i = 0; i < arreglo.Length; i++)
+            {
+                arreglo[i] = arreglo[i] * 10;
+            }
+
+            //C# tiene un metodo para imprimir colecciones
+            //sin necesidad de recorrerlas con for
+            Console.WriteLine( String.Join("\n",arreglo ) );
+            Console.WriteLine(String.Join(",", arreglo));
+            Console.WriteLine(String.Join(" ::: ", arreglo));
+        }
+    }
+}
---
+++ 33_String01/33_String01/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _33_String01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*tipo de dato String
+            Es una coleccion de elementos char.
+            -> Su tamaño depende de cuantos caracteres tiene almacenados
+            -> Cada caracter mide 1 byte dependiendo del alfabeto
+            -> En C# el tipo de dato String se basa en una clase de la
+               que se crean objetos.
+            -> En general String al español lo traducen como Cadena de Texto
+            -> en C# puede usar String (son S mayuscula) o string (en minuscula)
+               ambas palabras son aceptadas y hacen lo mimo.*/
+
+            //declarar un objeto string:
+            String str1;
+            string cadena1;
+            //string es un alias de String
+
+            //String es auto-instanciable o sea que no necesita de la
+            //palabra new para crear un nuevo objeto.
+            //instanciar = dar memoria a un objeto (asignarlo a la memoria RAM)
+            string mascota = "Firulais";
+            string campus;
+            campus = "UNAH Cortes";
+            string color = "rojo", carro = "Toyota", pais = "Honduras";
+
+            //en C# no se puede leer un string sin haberle asignado un valor
+            /*string algo;
+            Console.WriteLine($"valor de algo: {algo}");*/
+
+            //solucion: asignele un string en blanco
+            string algo = "";
+            Console.WriteLine($"valor de algo: {algo}");
+        }
+    }
+}
