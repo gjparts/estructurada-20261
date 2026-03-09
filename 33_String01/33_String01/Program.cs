6725b484d6865f4a0ca518b64a94baa89bd98b90
gjparts <gjparts@gmail.com>
2026-03-09 10:52:38 -0600

    commit 0903202611833

--- 33_String01/33_String01/Program.cs
+++ 33_String01/33_String01/Program.cs
@@ -34,6 +34,90 @@
             //solucion: asignele un string en blanco
             string algo = "";
             Console.WriteLine($"valor de algo: {algo}");
+
+            //Concatenar String
+            string fruta = "Manzana";
+            string color2 = "Verde";
+            string cadena3 = fruta + " " + color2;
+            Console.WriteLine($"{cadena3}");
+
+            //Otra forma de concatenar: por medio del interpolador $
+            string cadena4 = $"{fruta} {color2}";
+            Console.WriteLine($"{cadena4}");
+
+            //se puede almacenar numeros en un String?
+            //string s1 = 7; //por conversion implicita no se puede
+            //pero si aprovechamos la ley de precedencia de operadores
+            //podemos saltarnos la conversion implicita y hacerla
+            //una concatenacion
+            string s1 = "" + 6; //por medio de la inyeccion de string vacio
+            //otra forma es convertir el numero usando un metodo:
+            string s2 = 6.ToString();
+            //mas ejemplos:
+            string s3 = ""+2.456;
+            string s4 = 5.231.ToString();
+
+            //conocer la longitud de un string
+            string s5 = "UNAH Cortes";
+            Console.WriteLine($"s5: {s5}");
+            Console.WriteLine($"Longitud de s5: {s5.Length}");
+            //la propiedad Length devuelve un valor int el cual
+            //indica cuantos caracteres tiene un String
+
+            //dentro de un String cada caracter se enumera
+            //desde 0 hasta Length-1 (Zero indexing)
+            //para obtener un caracter de una posicion determinada
+            //se le da la posicion del char deseado la cual se pone
+            //entre corchetes (similar a como lo hace con un arreglo)
+            string s6 = "Honduras";
+            Console.WriteLine($"s6: {s6}");
+            Console.WriteLine($"Primer caracter de s6: {s6[0]}");
+            Console.WriteLine($"Caracter en la posicion 4: {s6[4]}");
+            Console.WriteLine($"Ultimo caracter de s6: {s6[7]}");
+            Console.WriteLine($"Ultimo caracter de s6: {s6[s6.Length-1]}");
+            Console.WriteLine($"Penultimo caracter de s6: {s6[s6.Length - 2]}");
+
+            string comida = "Pollo frito";
+            //haga un programa que imrima el string comida colocando
+            //cada uno de sus caracteres en un renglon diferente.
+            for(int i = 0; i < comida.Length; i++)
+            {
+                Console.WriteLine(comida[i]);
+            }
+
+            //en C# no es posible cambiar un elemento char que esta
+            //dentro de un String por medio del uso de corchetes:
+            //comida[1] = 'X';
+
+            //Transformaciones de String
+            //obtener una version en mayusculas o minusculas de un String
+            //metodos a usar: ToUpper(), ToLower()
+            //el usar estos metodos no altera al String original
+            string ciudad = "San Pedro Sula";
+            Console.WriteLine($"ciudad en mayusculas: {ciudad.ToUpper()}");
+            Console.WriteLine($"ciudad en minusculas: {ciudad.ToLower()}");
+            Console.WriteLine($"string original: {ciudad}");
+            //si Usted quisiera alterar la variable original, simplemente
+            //la debe sobreescribir con la copia:
+            ciudad = ciudad.ToUpper();
+            Console.WriteLine($"string original: {ciudad}");
+
+            //Metodo Trim()
+            //Devuelve una copia del String a la cual se le ha
+            //eliminado los espacios en blanco iniciales y finales
+            //tambien conocidos como espacios de relleno.
+            //-> Este metodo no afecta al String original.
+            string persona = "     Gerardo Portillo             ";
+            Console.WriteLine($"persona: *{persona}*");
+            Console.WriteLine($"Longitud de persona: {persona.Length}");
+            //impresion usando Trim:
+            Console.WriteLine($"Trim a persona: *{persona.Trim()}*");
+            Console.WriteLine($"Longitud de persona con Trim: {persona.Trim().Length}");
+            //imprimir la variable original:
+            Console.WriteLine($"persona: *{persona}*");
+            //deseo alterar la variable original y dejarla sin relleno:
+            persona = persona.Trim();
+            Console.WriteLine($"persona: *{persona}*");
         }
     }
 }
